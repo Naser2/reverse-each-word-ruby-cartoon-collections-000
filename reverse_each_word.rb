de81a37de7981678874c4c9e271404f85f1cb1f7
Nasser Sanou <nassersanou23@gmail.com>
2018-08-07 19:23:15 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -10,4 +10,13 @@ def reverse_each_word( sentence )
 reverse_each_word("Hello there, and how are you?" )
 
 
+ # -------Using ".collect" ---------------
+
+def reverse_collect_word(sentence)
+ reversed = []
+
+  reversed = sentence.split(" ").collect{ |word| word.reverse}
+return  reversed.join(" ")
+
+end
  reverse_collect_word("Hello there, and how are you?")
