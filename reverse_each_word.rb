999ea153eb1ae11655fc8524b97435195d692dd0
Nasser Sanou <nassersanou23@gmail.com>
2018-08-07 18:02:45 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,9 @@
-def reverse_each_word(sentence)
-  # sentence.split("")
-   sentence.each_word do |w| return w.reversed }
-   puts sentence 
+def reverse_each_word( sentence )
+ word = sentence.split
+  reversed = ""
+  reversed = word.each { |word| puts word.reverse }
+  return reversed 
+  # print reversed
 end 
+
+reverse_each_word("Hello there, and how are you?" )
