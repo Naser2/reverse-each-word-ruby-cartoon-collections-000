a38fe382d7e17cce4bdcaed5160da7278f8810d8
Nasser Sanou <nassersanou23@gmail.com>
2018-08-07 18:58:21 +0000

    automatically backed up by learn

--- reverse_collect_word.rb
+++ reverse_collect_word.rb
@@ -2,8 +2,8 @@ def reverse_collect_word( sentence )
   
 words = sentence.split 
   reversed = []
-words.collect{|word| puts reversed << word.reverse }
-return  reversed
+words.collect{|word| reversed << word.reverse }
+return  reversed.join(" ")
 
 end
  reverse_collect_word("Hello there, and how are you?")
